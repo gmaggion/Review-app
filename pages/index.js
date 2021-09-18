0ecd537c24b462372c5c4cade79005426be5d323
Ketlyn <merellketlyn1@gmail.com>
2021-09-18 11:26:01 -0300

    Review-app1

--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,40 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bem-vindo ao <a href="https://instagram.com/projetoceos">Projeto Céos</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          aprenda e se torne um dev{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Documentação &rarr;</h2>
+            <p>Base de conhecimento do Projeto</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="/review" className={styles.card}>
+            <h2>Avaliações &rarr;</h2>
+            <p>Um CRUD de avaliações</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Blog &rarr;</h2>
+            <p>Um exemplo de um Blog</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Sobre &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Um exemplo de página sobre
             </p>
           </a>
         </div>
